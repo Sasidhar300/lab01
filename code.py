89a92d8c1f697a7b7364934836922290157a2de1
Satya Nikhil Kodukula <satya@Satyas-MacBook.local>
2022-01-21 13:19:27 -0500

    added more validations

--- code.py
+++ code.py
@@ -1,8 +1,11 @@
 def are_valid_groups(students,groups):
     for x in students:
+        x = str(x)
         c = 0
         while c != 0 and c < 2 :
             for y in groups:
+                if len(y) not in [2,3]:
+                    c=2
                 for group_member in y :
                     if x == group_member :
                         c += 1
