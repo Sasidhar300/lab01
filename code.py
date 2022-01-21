7b02143ca4f9a65c24e926d0bfa02d9d09074e5f
Satya Nikhil Kodukula <satya@Satyas-MacBook.local>
2022-01-21 12:50:44 -0500

    Satya's python file

--- code.py
+++ code.py
@@ -1,1 +1,15 @@
-
+def are_valid_groups(students,groups):
+    for x in students:
+        c = 0
+        while c != 0 and c < 2 :
+            for y in groups:
+                for group_member in y :
+                    if x == group_member :
+                        c += 1
+                if c > 1:
+                    break
+            if c > 1:
+                break
+        if c != 1:
+            return False
+    return True
