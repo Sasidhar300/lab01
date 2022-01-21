bc158ac32e6d7b3decd4388ecc51fd4f0de533cf
ksh01 <62888715+ksh01@users.noreply.github.com>
2022-01-21 20:52:49 +0300

    Kartik's python file

--- code.py
+++ code.py
@@ -1,1 +1,18 @@
+def are_valid_groups(students, groups):
+    NumOfStudentsfound = 0
+    for student in students:
+        c = 0
+        for group in groups:
+            if student in group:
+                c += 1
 
+        if (c != 1):
+            return False
+        else:
+            NumOfStudentsfound += 1
+    
+    if NumOfStudentsfound == len(students):
+        return True
+    else:
+        return False
+        
